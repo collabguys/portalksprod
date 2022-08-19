8074f39840ee0111d9b1557cc987821cb44ae10b
felicita2031 <felicita2031@gmail.com>
2022-08-19 13:46:38 +0300

    yeni proje ilk commit3

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import App from '../../../project5/src/App';
+import App from '../../../portalksprod/src/App';
 
 test('renders learn react link', () => {
   render(<App />);
